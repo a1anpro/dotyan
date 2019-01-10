577f067466e7f6afedc22d6880dca9bfd0a96ecc
yanhui <alanyanhui@gmail.com>
2019-01-10 14:41:22 +0000

    refresh .vimrc and sh

---
+++ sh/install.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+
+apt-get -y install build-essential    # build-essential packages, include binary utilities, gcc, make, and so on
+apt-get -y install man                # on-line reference manual
+apt-get -y install gcc-doc            # manual for GCC
+apt-get -y install gdb                # GNU debugger
+apt-get -y install git                # reversion control system
+apt-get -y install libreadline-dev    # a library to use compile the project later
+apt-get -y install libsdl2-dev        # a library to use compile the project later
+apt-get -y install qemu-system-x86    # QEMU
+
+
+
+echo -e "\033[31m 安装完成  \033[0m" 
